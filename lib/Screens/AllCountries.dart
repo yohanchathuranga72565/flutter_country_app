2e25cd4ffa251a687a28ef0dcb029e6ab81159f7
Yohan Chathuranga <yohanchathuranga72565@gmail.com>
2021-09-07 18:57:03 +0530

    Create list view of country staticaly

--- lib/Screens/AllCountries.dart
+++ lib/Screens/AllCountries.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:country_app/Screens/Country.dart';
 
 class AllCountries extends StatelessWidget {
   @override
@@ -10,8 +11,51 @@ class AllCountries extends StatelessWidget {
         title: Text('All Countries'),
       ),
       body: Container(
-        child: Text('Hello'),
-      ),
+          padding: EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) {
+                      return Country();
+                    }),
+                  );
+                },
+                child: Card(
+                  elevation: 10,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                    child: Text(
+                      'India',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) {
+                      return Country();
+                    }),
+                  );
+                },
+                child: Card(
+                  elevation: 10,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                    child: Text(
+                      'Canada',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
